7e27d5add69cc447d0c098b6b2a166666eb9ed8e
lilithchen <lilithchen68@gmail.com>
2024-12-04 02:44:09 +0800

    init and add swiper

---
+++ components/swiper/Swiper.vue
@@ -0,0 +1,40 @@
+<script setup>
+// Swiper JS 核心功能
+import Swiper from "swiper";
+// Swiper 核心與模組的樣式
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// 使用 defineProps() 設定以下 props 接受來自父元件的參數
+const props = defineProps({
+  autoplay: Object,
+  pagination: Object,
+  navigation: Object,
+  loop: Boolean,
+  slidesPerView: Number,
+  modules: Array,
+  breakpoints: Object,
+});
+
+// 取得輪播容器的 DOM 節點
+const swiperRef = ref(null);
+// 在 onMounted() 生命週期把 DOM 節點與元件的 props 傳入 new Swiper()
+onMounted(() => {
+  new Swiper(swiperRef.value, props);
+});
+</script>
+
+<template>
+  <div class="swiper" ref="swiperRef">
+    <div class="swiper-wrapper">
+      <slot />
+    </div>
+    <div class="swiper-pagination"></div>
+
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<script setup>
+const { data: rooms } = await useFetch("/api/v1/rooms", {
+  baseURL: "https://nuxr3.zeabur.app",
+});
+
+// Swiper 模組
+// 因為不一定每個 Swiper 使用的模組都一樣，所以改成用 props 傳入特定的模組
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+</script>
+
+<template>
+  <div class="container mt-5">
+    <ul class="list-unstyled">
+      <li
+        v-for="room in rooms.result"
+        :key="room._id"
+        class="card flex-lg-row mb-3"
+      >
+        <div class="row">
+          <div class="col-lg-6">
+            <Swiper
+              :loop="true"
+              :slides-per-view="1"
+              :pagination="{ el: '.swiper-pagination', type: 'bullets' }"
+              :navigation="{
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              }"
+              :autoplay="{ delay: 5000 }"
+              :modules="[Navigation, Pagination, Autoplay]"
+            >
+              <SwiperSlide v-for="img in room.imageUrlList" :key="img">
+                <img :src="img" class="card-img-top" alt="..." />
+              </SwiperSlide>
+            </Swiper>
+          </div>
+          <div class="col-lg-6">
+            <div class="card-body">
+              <h3 class="card-title fs-2 fw-bold">
+                {{ room.name }}
+              </h3>
+              <p class="card-text fw-medium">
+                {{ room.description }}
+              </p>
+              <ul class="d-flex gap-4 list-unstyled mb-5">
+                <li class="card-info border rounded-3 p-3">
+                  <Icon
+                    class="mb-2 fs-5 text-primary-100"
+                    icon="fluent:slide-size-24-filled"
+                  />
+                  <p class="mb-0 fw-bold text-neutral-80 text-nowrap">
+                    {{ room.areaInfo }}
+                  </p>
+                </li>
+                <li class="card-info border rounded-3 p-3">
+                  <Icon class="mb-2" icon="material-symbols:king-bed" />
+                  <p class="mb-0 fw-bold">
+                    {{ room.bedInfo }}
+                  </p>
+                </li>
+                <li class="card-info border rounded-3 p-3">
+                  <Icon class="mb-2" icon="ic:baseline-person" />
+                  <p class="mb-0 fw-bold">2-{{ room.maxPeople }} 人</p>
+                </li>
+              </ul>
+              <p class="mb-0 fw-bold">NT$ {{ room.price }}</p>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
